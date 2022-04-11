3b6e784dfdc1fdad8cd21434b1d4735e07eb6fcc
David Goosen <deadVertex@gmail.com>
2022-04-11 19:23:45 +1200

    Add spawning of bullet holes when shooting

--- scenes/prefabs/player.gd
+++ scenes/prefabs/player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+const BULLET_DISTANCE: float = 100.0
+const BULLET_HOLE_OFFSET: float = 0.001
 export var camera_sensitivity: float = 0.05
 export var speed: float = 13.0
 export var acceleration: float = 6.0
@@ -10,6 +12,9 @@ onready var head: Spatial = $Head
 onready var uzi_animations: AnimationPlayer = $Head/ViewModel/AnimationPlayer
 
 var velocity: Vector3 = Vector3.ZERO
+
+# TODO: Make this editor configurable
+var _bullet_hole_prefab = preload("res://scenes/prefabs/bullet_hole.tscn")
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -26,6 +31,7 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector3.UP)
 
 	if Input.is_action_pressed("fire"):
+		_handle_shooting()
 		uzi_animations.play("Uzi_Fire")
 	else:
 		uzi_animations.play("Uzi_Idle")
@@ -38,6 +44,24 @@ func _handle_camera_rotation(event):
 	rotate_y(deg2rad(-event.relative.x * camera_sensitivity))
 	head.rotate_x(deg2rad(-event.relative.y * camera_sensitivity))
 	head.rotation.x = clamp(head.rotation.x, -PI * 0.5, PI * 0.5);
+
+func _handle_shooting():
+	var space_state = get_world().direct_space_state
+	var start = head.global_transform.origin
+	var end = start - head.global_transform.basis.z * BULLET_DISTANCE
+	var result = space_state.intersect_ray(start, end)
+	if !result.empty():
+		_spawn_bullet_hole(result["position"], result["normal"])
+
+func _spawn_bullet_hole(position: Vector3, normal: Vector3):
+	var bullet_hole = _bullet_hole_prefab.instance()
+	var up = Vector3.UP
+	if normal.dot(up) >= 0.99999:
+		up = Vector3.RIGHT
+
+	bullet_hole.look_at_from_position(position + normal * BULLET_HOLE_OFFSET,
+									  position + normal, up)
+	get_tree().get_root().add_child(bullet_hole)
 
 func _get_movement_direction():
 	var direction = Vector3.DOWN
