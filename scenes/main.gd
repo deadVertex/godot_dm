4bedc61e1c60e300feff1d0f1141090ffc7b9f07
David Goosen <david@davidgoosen.kiwi>
2022-04-14 13:12:11 +1200

    Add main scene

---
+++ scenes/main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+export var map: PackedScene
+onready var _world = $World
+
+func _ready():
+	_load_map()
+
+func _load_map():
+	assert(map)
+	var scene = map.instance()
+	_world.add_child(scene)	
