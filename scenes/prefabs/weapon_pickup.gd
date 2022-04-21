552e7cd957925cdf3a2ac68666963581ce15e2a9
David Goosen <david@davidgoosen.kiwi>
2022-04-21 15:42:10 +1200

    Work on respawning weapon pickups

--- scenes/prefabs/weapon_pickup.gd
+++ scenes/prefabs/weapon_pickup.gd
@@ -6,6 +6,24 @@ const ViewModel = preload("res://scenes/prefabs/view_model.gd")
 
 export var weapon: int = ViewModel.Weapon.UZI
 
+var _weapon_available := true
+
+onready var _weapon_origin: Spatial = $WeaponOrigin
+
 func _on_area_body_entered(body: Player) -> void:
 	if body is Player:
 		body.give_weapon(weapon)
+		set_weapon_available(false)
+
+
+func set_weapon_available(available: bool) -> void:
+	_weapon_available = available
+	_weapon_origin.visible = available
+
+func get_weapon_available() -> bool:
+	return _weapon_available
+
+
+func _on_respawn_timer_timeout() -> void:
+	print("_on_respawn_timer_timeout")
+	set_weapon_available(true)
--- tests/test_weapon_pickup.gd
+++ tests/test_weapon_pickup.gd
@@ -23,3 +23,48 @@ func test_overlap_area():
 
 	# Then the player receives the weapon from the pickup
 	assert_called(player, "give_weapon", [ViewModel.Weapon.SHOTGUN])
+
+
+func test_weapon_available():
+	# Given a weapon pickup
+	var weapon_pickup = add_child_autofree(WeaponPickup.instance())
+	weapon_pickup.weapon = ViewModel.Weapon.SHOTGUN
+	assert_eq(true, weapon_pickup.get_weapon_available())
+
+	# When a player overlaps with the pickup
+	var player = add_child_autofree(double(Player).instance())
+	var area = weapon_pickup.get_node("Area")
+	# TODO: Replace with moving player into area?
+	area.emit_signal("body_entered", player)
+
+	# Then the weapon is no longer available
+	assert_eq(false, weapon_pickup.get_weapon_available())
+
+
+func test_weapon_meshes_hidden_when_not_available():
+	# Given a weapon pickup
+	var weapon_pickup = add_child_autofree(WeaponPickup.instance())
+	weapon_pickup.weapon = ViewModel.Weapon.SHOTGUN
+	var shotgun_mesh = weapon_pickup.get_node("WeaponOrigin/ShotgunMesh")
+	assert_eq(true, shotgun_mesh.visible)
+
+	# When we mark the weapon as unavailable
+	weapon_pickup.set_weapon_available(false)
+
+	# Then the weapon meshes are hidden
+	assert_eq(false, shotgun_mesh.is_visible_in_tree())
+
+
+# NOTE: This should only run on the client!
+func test_weapon_respawn():
+	# Given a weapon pickup which is unavailable
+	var weapon_pickup = add_child_autofree(WeaponPickup.instance())
+	weapon_pickup.weapon = ViewModel.Weapon.SHOTGUN
+	weapon_pickup.set_weapon_available(false)
+
+	# When the respawn timer times out
+	var respawn_timer = weapon_pickup.get_node("RespawnTimer")
+	respawn_timer.emit_signal("timeout")
+
+	# Then the weapon is available again
+	assert_eq(true, weapon_pickup.get_weapon_available())
